f2abf6290e4d65f9d20b9089b6207ecbb25e7819
Samuel ROZE <samuel.roze@gmail.com>
2015-11-12 11:27:42 +0000

    Open to the world and on port 80

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
-    hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 4567,
+    hostname = process.env.HOSTNAME || '0.0.0.0',
+    port = parseInt(process.env.PORT, 10) || 80,
     publicDir = process.argv[2] || __dirname + '/public';
 
 app.get("/", function (req, res) {
