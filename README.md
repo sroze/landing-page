7fc60e6fd0a668861c120df2c8e05e9694de14fb
Samuel ROZE <samuel.roze@gmail.com>
2017-03-12 14:18:16 +0000

    Allow to configure the displayed message

---
+++ README.md
@@ -0,0 +1,7 @@
+# Landing page
+
+This is a very simple landing page.
+
+```
+docker run -e MESSAGE=hello docker.io/sroze/landing-page:latest
+```
--- server.js
+++ server.js
@@ -5,11 +5,27 @@ var express = require("express"),
     methodOverride = require('method-override'),
     hostname = process.env.HOSTNAME || '0.0.0.0',
     port = parseInt(process.env.PORT, 10) || 80,
-    publicDir = process.argv[2] || __dirname + '/public';
+    publicDir = process.argv[2] || __dirname + '/public',
+    message = process.env.MESSAGE || "Please be patient. We're crafting something for you.";
 
-app.get("/", function (req, res, next) {
-  req.url = '/index.html';
-  next();
+app.get("/", function (req, res) {
+  res.status(200).send(
+      "<!doctype html>"+
+      "<html class=\"no-js\">"+
+      "<head>"+
+      "    <meta charset=\"utf-8\">"+
+      "    <base href=\"/\">"+
+      "    <title>"+message+"</title>"+
+      "    <meta name=\"description\" content=\"\">"+
+      "    <meta name=\"viewport\" content=\"width=device-width\">"+
+      "    <link rel=\"stylesheet\" href=\"styles/main.css\">"+
+      "    <link href=\"https://fonts.googleapis.com/css?family=Roboto:400,500,700\" rel=\"stylesheet\" type=\"text/css\">"+
+      "</head>"+
+      "<body>"+
+      "  <div class=\"message\">"+message+"</div>"+
+      "</body>"+
+      "</html>"
+    );
 });
 
 app.use(methodOverride());
